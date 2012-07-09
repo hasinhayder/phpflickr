a916b583b6fe9ccc5c964ac3316dd979e21e3bb2
Hasin Hayder <ironhide@leevio.com>
2012-07-09 15:42:47 +0600

    declaration fixed

--- flickruserdata.php
+++ flickruserdata.php
@@ -13,6 +13,11 @@ include_once("objectbroker.php");
 include_once("requestsigner.php");
 include_once("httprequest.php");
 include_once("datastore.php");
+$fl = new FlickrLib("8cc0f91339ab3da808b215c72a3d564d", //key
+    "54414ae0140a3903", //secret
+    "http://grasshopper.me/f2/flickruserdata.php", //where it will be redirected after successful login
+    "read" //permission
+);
 if($fl->auth->isLoggedIn()){
     echo "Logged in<br/>";
     echo "Signed API Response: ". $fl->api->testlogin();
